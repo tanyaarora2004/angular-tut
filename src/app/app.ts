ccb2d3d145b153b36171d1cca3301675b493b9a8
Tanya <artanya1012@gmail.com>
2025-06-16 17:48:26 +0530

    component_function_call

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Login,Signup,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = 'angular-tut';
-  name='Tanya'
+//   title = 'angular-tut';
+//   name='Tanya'
+
+// function call on button click 
+
+// handleClickEvent(){
+//   console.log(  "Function called");
+//   this.otherFunction()
+// }
+// otherFunction(){
+//   console.log("other called");
+  
+// }
+
+// define data types
+
+name:string="Tanya Arora"
+data:string|number="tanya"
+other:any=false
+updateName(){
+// this.name="Peter"  
+this.name="20"
+this.data=123
+}
+
+sum(a:number,b:number){
+  console.log(a+b);
+  
+}
 }
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector:'app-profile',
+    // template:`<h1>Profile Component</h1>`,  // if there are single line
+    // styles:'h1{color:orange}'
+    templateUrl:'./profile.html',
+    styleUrl:'./profile.css'
+})
+
+export class Profile{
+    
+}
