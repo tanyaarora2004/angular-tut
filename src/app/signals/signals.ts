ae854dc5e87ba0efdcd0ba0ee70c52b4ebc161a2
Tanya <artanya1012@gmail.com>
2025-06-22 00:01:55 +0530

    signal data type

--- src/app/signals/signals.ts
+++ src/app/signals/signals.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -7,23 +7,35 @@ import { Component, effect, signal } from '@angular/core';
   styleUrl: './signals.css'
 })
 export class Signals {
-  count=signal(10);
-  x=20
-  constructor(){
-    effect(()=>{
-      // console.log(this.x)    // property
-      console.log(this.count());
+  // count=signal(10);
+  // x=20
+  // constructor(){
+  //   effect(()=>{
+  //     // console.log(this.x)    // property
+  //     console.log(this.count());
       
-    })
-  }
-  update(val:string){
-    // this.x=30;   //property
-    // this.count.set(100)   //signal
+  //   })
+  // }
+  // update(val:string){
+  //   // this.x=30;   //property
+  //   // this.count.set(100)   //signal
 
-    if(val=='dec'){
-      this.count.set(this.count()-1);
-    }else{
-      this.count.set(this.count()+1)
-    }
+  //   if(val=='dec'){
+  //     this.count.set(this.count()-1);
+  //   }else{
+  //     this.count.set(this.count()+1)
+  //   }
+  // }
+
+
+
+  // data types with signals
+
+  // data : WritableSignal<number | string>=signal<number | string>(10);
+  data : WritableSignal<number>=signal<number>(10);
+  count:Signal<number>=computed(()=>10)
+  update(){
+    // this.data.set("hii");
+    this.data.update((val)=>val+1);
   }
 }
