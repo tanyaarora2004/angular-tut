3226ef3b8d1942442a1c1a7ada869c970cf350de
Tanya <artanya1012@gmail.com>
2025-07-03 00:59:25 +0530

    structured directives

--- src/app/directives/directives.ts
+++ src/app/directives/directives.ts
@@ -1,29 +1,45 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [NgIf,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './directives.html',
   styleUrl: './directives.css'
 })
 export class Directives {
   // show=true
 
-  students=["Tanya","Vanya","Sanya","Shanaya","Manya","Gurpreet"]
-   studentsData=[
-    {
-      name:"Tanya",age:20,email:"tanya@gmail.com"
-    },
-    {
-      name:"Vanya",age:21,email:"vanya@gmail.com"
-    },
-    {
-      name:"Manya",age:10,email:"manya@gmail.com"
-    },
-    {
-      name:"Sanya",age:11,email:"sanya@gmail.com"
-    },
-  ]
+  // login=false
+  // block=0
+  // students=["Tanya","Vanya","Sanya","Shanaya","Manya","Gurpreet"]
+  //  studentsData=[
+  //   {
+  //     name:"Tanya",age:20,email:"tanya@gmail.com"
+  //   },
+  //   {
+  //     name:"Vanya",age:21,email:"vanya@gmail.com"
+  //   },
+  //   {
+  //     name:"Manya",age:10,email:"manya@gmail.com"
+  //   },
+  //   {
+  //     name:"Sanya",age:11,email:"sanya@gmail.com"
+  //   },
+  // ]
+
+  // updateBlock(){
+  //   this.block++;
+  // }
+
+
+  // ngSwitch
+
+  color="";
+
+  changeColor(color:string){
+    this.color=color
+  }
+
 }
